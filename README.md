d039c29393d67909c6663ac0644cdb035173eac4
apartapatia <raupov.deal@yandex.ru>
2026-03-22 12:30:13 +0300

    add process logic

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # devops-elective-itmo-2026-1lab
+
+## Список вопросов для самостоятельного исследования и ответов
+
+запуск как parent потом как child? 
+
+
+# Источники
+
+https://github.com/lizrice/containers-from-scratch/blob/master/main.go
+
--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"os"
+	"os/exec"
+	"syscall"
 )
+
+func generateID() string {
+	b := make([]byte, 6)
+	rand.Read(b)
+	
+	return hex.EncodeToString(b)
+}
 
 type Config struct {
 	Version  string    `json:"ociVersion"` 
@@ -58,6 +69,48 @@ type Linux struct {
 	} `json:"namespaces"`
 }
 
+func stdConnect(cmd *exec.Cmd) {
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+}
+
+func run(config Config, containerId string) {
+	exeLinuxPath, err := os.Executable()
+	if err  != nil {
+		fmt.Printf("Ошибка получаения exe path: %v\n", err)
+		os.Exit(1)
+	}
+	
+	args := append([]string{"child", containerId}, config.Process.Args...)
+	
+	cmd := exec.Command(exeLinuxPath, args...)
+	
+	stdConnect(cmd)
+	
+	cmd.SysProcAttr = &syscall.SysProcAttr {
+		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS,
+	}
+	
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Ошибка запуска parent процесса: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func child(config Config, containerId string) { // containerId зачем тут он нужен?
+	cmd := exec.Command(config.Process.Args[0], config.Process.Args[1:]...)
+	
+	stdConnect(cmd)
+	
+	syscall.Sethostname([]byte(config.Hostname))
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Ошибка запуска child процесса: %v\n", err)
+		os.Exit(1)
+	}
+}
+
 func main() {
 	configJson, err := os.Open("config.json")
 	if err != nil {
@@ -77,4 +130,15 @@ func main() {
 	fmt.Println("Успешное открытие конфига 🎉")
 	fmt.Printf("HOSTNAME: %s\n", config.Hostname)
 	fmt.Printf("EXECUTABLE PROGRAMM: %v\n", config.Process.Args)
+	
+	switch os.Args[1] {
+		case "run":
+		    containerId := generateID()
+			run(config, containerId)
+		case "child":
+		    containerId := os.Args[2]
+			child(config, containerId)
+		default:
+			panic("я упал")
+		}
 }
